647835df28fdbd83e382576b898cca7a1f3c0105
Seyyed Ali Mohammadiyeh (MAX BASE) <MaxBaseCode@gmail.com>
2025-10-25 23:10:59 +0330

    add config

---
+++ config.example.php
@@ -0,0 +1,20 @@
+<?php
+$transcript = [
+    [
+        "year" => "1398-1397",
+        "semester" => "اول",
+        "courses" => [
+            ["code" => "93107", "name" => "فارسی عمومی", "units" => 3, "grade" => 20.00],
+            ["code" => "93125", "name" => "اخلاق اسلامی (مبانی و مفاهیم)", "units" => 2, "grade" => 20.00],
+            ["code" => "110100", "name" => "ریاضی مقدماتی", "units" => 4, "grade" => 20.00],
+            ["code" => "110221", "name" => "مبانی ریاضی", "units" => 3, "grade" => 20.00],
+            ["code" => "111702", "name" => "مبانی ترکیبیات", "units" => 3, "grade" => 20.00],
+        ],
+        "term_units_taken" => 15,
+        "term_units_passed" => 15,
+        "term_avg" => 20.00,
+        "total_units_taken" => 15,
+        "total_units_passed" => 15,
+        "total_avg" => 20.00,
+    ],
+];
